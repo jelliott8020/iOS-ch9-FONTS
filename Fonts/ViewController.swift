a8ed1b5a9b9ebc5a61f81649cac90f2904468ae8
Josh Elliott <josh@joshs-macbook-pro.local>
2018-10-28 11:28:00 -0400

    Initial Commit

---
+++ Fonts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fonts
+//
+//  Created by Josh Elliott on 10/28/18.
+//  Copyright © 2018 JoshElliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
