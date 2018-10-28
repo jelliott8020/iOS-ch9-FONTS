04f3a207870bc045883ff90a6ba1b35f8c8864df
Josh Elliott <josh@joshs-macbook-pro.local>
2018-10-28 13:31:20 -0400

    Break time. Page 328 Section "Creating the Font Sizes View Controller"

---
+++ Fonts/FavoritesList.swift
@@ -0,0 +1,42 @@
+//
+//  FavoritesList.swift
+//  Fonts
+//
+//  Created by Josh Elliott on 10/28/18.
+//  Copyright © 2018 JoshElliott. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesList {
+    
+    static let sharedFavoritesList = FavoritesList()
+    private(set) var favorites:[String]
+    
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavorites = defaults.object(forKey: "favorites") as? [String]
+        favorites = storedFavorites != nil ? storedFavorites! : []
+    }
+    
+    func addFavorite(fontName: String) {
+        if !favorites.contains(fontName) {
+            favorites.append(fontName)
+            saveFavorites()
+        }
+    }
+    
+    func removeFavorite(fontName: String) {
+        if let index = favorites.index(of: fontName) {
+            favorites.remove(at: index)
+            saveFavorites()
+        }
+    }
+    
+    private func saveFavorites() {
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        defaults.synchronize()
+    }
+}
